765b02ace6d80ec7de3eafdb582d19115bf3117a
Albana Romina Schiffini <95891550+arsalbana@users.noreply.github.com>
2022-07-19 23:59:35 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import './favicon-32x32.png'
+
+
+function App() {
+  return (
+    <div className="App">
+        <div className='nav'>
+          
+          <ul className='items'>
+          <img src={'./favicon-32x32.png'} />
+            <li>
+              Nosotros
+            </li>
+            <li>
+              Productos
+            </li>
+            <li>
+              consultas
+            </li>
+            
+          </ul>
+        </div>
+    </div>
+      
+    
+  );
+}
+
+export default App;
+
